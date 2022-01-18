33af7564b59acea65dc081dbdd82180d4f378bb4
Ümit Öztürk <ozturkumit34@gmail.com>
2022-01-18 23:11:30 +0300

    feature(#10): create player-new component under player module

---
+++ app/components/player/player-new.js
@@ -0,0 +1,31 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class PlayerNewComponent extends Component {
+  @service store;
+  @service router;
+
+  playerName = '';
+
+  @action
+  async registerPlayer() {
+    this.error = [];
+    let player = this.store.createRecord('player', {
+      name: this.playerName,
+    });
+    try {
+      await player.save();
+      this.playerIndexPage();
+    } catch (e) {
+      this.store.unloadRecord(player);
+      this.error = e.errors;
+      console.log(this.error);
+    }
+  }
+
+  @action
+  playerIndexPage() {
+    this.router.transitionTo('/player');
+  }
+}
--- app/controllers/player/new.js
+++ app/controllers/player/new.js
@@ -1,31 +1,3 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
-
 export default class PlayerNewController extends Controller {
-  @service store;
-  @service router;
-
-  playerName = '';
-
-  @action
-  async registerPlayer() {
-    this.error = [];
-    let player = this.store.createRecord('player', {
-      name: this.playerName,
-    });
-    try {
-      await player.save();
-      this.playerIndexPage();
-    } catch (e) {
-      this.store.unloadRecord(player);
-      this.error = e.errors;
-      console.log(this.error);
-    }
-  }
-
-  @action
-  playerIndexPage() {
-    this.router.transitionTo('/player');
-  }
 }
