fefaae7265160cb7b82ddf5e3b27812056c922f9
Ümit Öztürk <ozturkumit34@gmail.com>
2022-01-18 23:01:05 +0300

    feature(#10): add player create page

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,8 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
+import { underscore } from '@ember/string';
 
 export default class ApplicationSerializer extends JSONAPISerializer {
+  keyForAttribute(attr) {
+    return underscore(attr);
+  }
 }
