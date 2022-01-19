54450f84b6e70e555b05572ef4e189b93495a111
Ümit Öztürk <ozturkumit34@gmail.com>
2022-01-19 03:05:12 +0300

    add board selection component

--- app/controllers/player/index.js
+++ app/controllers/player/index.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
 
 export default class PlayerIndexController extends Controller {
   @service router;
+
+  @alias('model')
+  players;
 
   @action
   homePage() {
